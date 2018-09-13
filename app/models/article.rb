4eea1f3bccd9b4ba2e0a03117b32c76b9d01bc34
Rails Alpha Blog <ajh5150@gmail.com>
2018-09-13 01:37:15 -0400

    create new template, action and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
- validates :title, presence: true, lenght: { minimum: 3, maximum: 50}
- validates :description, presence: true, lenght: { minimum: 10, maximum: 300}
+ validates :title, presence: true, length: { minimum: 3, maximum: 50}
+ validates :description, presence: true, length: { minimum: 10, maximum: 300}
   
   
   
