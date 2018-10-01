3b2fd641537362c9c17777ce192b67515f10aa0e
Rails Alpha Blog <ajh5150@gmail.com>
2018-10-01 11:54:24 -0400

    add user validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,10 @@
 class Article < ApplicationRecord
- validates :title, presence: true, length: { minimum: 3, maximum: 50}
- validates :description, presence: true, length: { minimum: 10, maximum: 300}
-  
-  
-  
-  
+ validates :title, presence: true, 
+            uniqueness: {case_sensitive: false} , 
+            length: { minimum: 3, maximum: 50}
+ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+ validates :email, presence: true, 
+            uniqueness: {case_sensitive: false} , 
+            format: {with: VALID_EMAIL_REGEX} , 
+            length: { maximum: 105}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  
+  validates :username, presence: true, length: { minimum: 3, maximum: 25}
+  validates :description, presence: true, length: { minimum: 10, maximum: 300}
   
   
   
