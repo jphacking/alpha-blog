a8d90e7a41e7cf357250f5ea6609ff37a5c95387
Rails Alpha Blog <ajh5150@gmail.com>
2018-10-08 18:01:46 -0400

    Signup users from UI

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end 
+  
+  def create
+    @user = User.new(user_params) 
+    if @user.save
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to articles_path(@user)
+    else
+      render 'new'
+    end
+  end 
+  
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+  
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181004220555) do
+ActiveRecord::Schema.define(version: 20181008211649) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20181004220555) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.         "password_digest"
   end
 
 end
